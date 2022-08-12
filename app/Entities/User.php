47fdfef3a141dec3592a35e9ca5e35471e158757
77web <info@77-web.com>
2022-08-12 09:42:04 +0900

    implemented Laravel's Authenticable to User Entity

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace App\Entities;
 
 use Doctrine\ORM\Mapping as ORM;
+use Illuminate\Contracts\Auth\Authenticatable;
 
 #[ORM\Entity]
-class User
+class User implements Authenticatable
 {
     #[ORM\Id]
     #[ORM\Column(type: 'integer')]
@@ -19,4 +20,37 @@ class User
 
     #[ORM\Column(type: 'string', length: 255)]
     public ?string $password = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    public ?string $rememberToken = null;
+
+    public function getAuthIdentifierName()
+    {
+        return 'id';
+    }
+
+    public function getAuthIdentifier()
+    {
+        return $this->id;
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRememberToken()
+    {
+        return $this->rememberToken;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->rememberToken = (string) $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
 }
