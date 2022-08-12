9439301a85c616af2e0393fbe238e2c90d9c093f
77web <info@77-web.com>
2022-08-12 09:21:05 +0900

    設定を調整

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Controllers\TodoController::class, 'index'])->name('todo.index');
+Route::get('/new', [Controllers\TodoController::class, 'new'])->name('todo.new');
+Route::post('/create', [Controllers\TodoController::class, 'create'])->name('todo.create');
